608cfb5318a93d6f311d796177201131f5dfbe99
Brandon <pacewicbrandon@gmail.com>
2021-11-09 09:18:06 -0800

    md file update

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# BalancedTernaryConverter
+# Balanced Ternary Converter
 
 ## Version 1.1
 
 ----
 
+Binary is the representation of numbers in bace two, Shown as 1s and 0s
+
+The number 5 in binary is represented as 101 as 5 contains one 4 and one 1
+
+2^2 2^1 2^0
+
+----
--- ternaryConverterv1.1/ternaryConverter.cpp
+++ ternaryConverterv1.1/ternaryConverter.cpp
@@ -8,25 +8,15 @@ using namespace std;
 int main() {
     int x;
     cin >> x;
-    // cout << pow(0, 3) << '\n';
-    // cout << 5 > x * float(2)/float(3) << '\n';
 
     vector<int> values;
-    int size = 0;
-    for (int i = 0; pow(3, i) < ((x * float(2)/float(3)) + 1); i++) {
+    for (int i = 0; pow(3, i) <= x; i++) {
         values.push_back(pow(3, i));
         cout << "worked " << pow(3, i) << '\n';
-        size++;
     }
 
-    if (values[size - 1] < (x * float(2)/float(3))) {
-        values.push_back(pow(3, size));
-        cout << "worked " << pow(3, size) << '\n';
-        size++;
-    }
-
-    vector<int> ternery(size, 0);
-    for (int i = size - 1; i >= 0; i--){
+    vector<int> ternery(values.size(), 0);
+    for (int i = values.size() - 1; i >= 0; i--){
         for (int k = 0; k < 2; k++)
             if (x >= values[i]) {
                 x -= values[i];
