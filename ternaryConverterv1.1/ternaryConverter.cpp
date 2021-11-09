6b596f582dd0a1f7b8e66d45f86757d356cc1703
Brandon <pacewicbrandon@gmail.com>
2021-11-09 09:51:48 -0800

    update to readme.md

--- ternaryConverterv1.1/ternaryConverter.cpp
+++ ternaryConverterv1.1/ternaryConverter.cpp
@@ -3,27 +3,23 @@
 using namespace std;
 
 
-//fix things please think about last night
-
 int main() {
     int x;
     cin >> x;
 
     vector<int> values;
-    for (int i = 0; pow(3, i) <= x; i++) {
+    for (int i = 0; pow(3, i) <= x; i++) 
         values.push_back(pow(3, i));
-        cout << "worked " << pow(3, i) << '\n';
-    }
 
-    vector<int> ternery(values.size(), 0);
-    for (int i = values.size() - 1; i >= 0; i--){
+    vector<int> ternary(values.size(), 0);
+    for (int i = values.size() - 1; i >= 0; i--) {
         for (int k = 0; k < 2; k++)
             if (x >= values[i]) {
                 x -= values[i];
-                ternery[i] += 1;
+                ternary[i] += 1;
             }
     }
 
-    for (auto i: ternery)
-        cout << i;
+    for (int i = ternary.size() - 1; i >= 0; i++) 
+        cout << ternary[i];
 }
