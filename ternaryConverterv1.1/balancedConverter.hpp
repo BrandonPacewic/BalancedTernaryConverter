2c87e1aa2e773e4997aa8a671c8753ae993af759
Brandon <pacewicbrandon@gmail.com>
2021-11-12 15:37:03 -0800

    removed dbg

--- ternaryConverterv1.1/balancedConverter.hpp
+++ ternaryConverterv1.1/balancedConverter.hpp
@@ -1,22 +1,19 @@
 template <typename T> T balancedConverter(T conVal) {
-    int64_t inVal = conVal;
+    int64_t inVal = int64_t(conVal);
     vector<int64_t> vals;
-    for (int i = 0; pow(3, i) <= conVal; i++) {
+    for (int i = 0; pow(3, i) <= conVal; i++) 
         vals.push_back(int64_t(pow(3, i)));
-        cout << "added " << pow(3, i) << '\n';
-    }
 
     sort(vals.begin(), vals.end(), greater<int64_t>());
 
     vector<int> balTer(vals.size(), 0);
     for (int i = 0; i < vals.size(); i++) {
-        for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++) 
             if (inVal >= vals[i]) {
                 balTer[i]++;
                 inVal -= vals[i]; 
-                cout << "removed " << vals[i] << '\n';
             }
-        }
+        
 
         // if (balTer[i] == 3) {
         //     balTer[i] = 0;
