7dfce10cb172a5535d83197c3de78bd15ea22de1
Brandon <pacewicbrandon@gmail.com>
2021-12-02 09:21:24 -0800

    file name change and dir change

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include "ternaryConverterv1.1/balancedConverter.hpp"
+using namespace std;
+
+int main() {
+    BALANCED_TERNARY_CONVERTER balancedTernaryConverter;
+    int64_t input;
+    cin >> input;
+
+    balancedTernaryConverter.convert(input);
+}
