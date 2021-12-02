b40bcc94b69af64c5461238531467d7b0c981b55
Brandon <pacewicbrandon@gmail.com>
2021-12-02 09:00:40 -0800

    added private class divisor

--- ternaryConverterv1.1/balancedConverter.hpp
+++ ternaryConverterv1.1/balancedConverter.hpp
@@ -1,48 +1,94 @@
-template<typename T> T balancedConverter(T conVal) {
-    int64_t inVal = int64_t(conVal);
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
 
-    bool neg = false;
-    if (inVal < 0) {
-        neg = true;
-        inVal *= -1;
+class BALANCED_TERNARY_CONVERTER {
+
+private: 
+    template<typename T> 
+    T abs(T T_Type) { 
+        return T_Type < 0 ? T_Type *= -1 : T_Type; 
     }
 
-    vector<int64_t> vals;
-    for (int i = 0; pow(3, i) <= conVal; i++) 
-        vals.push_back(int64_t(pow(3, i)));
-        
-    vals.push_back(int64_t(pow(3, vals.size())));
-    sort(vals.begin(), vals.end(), greater<int64_t>());
+    template<typename T_Num>
+    std::vector<int64_t> initalizeValues(T_Num ValueToBeConverted) {
+        std::vector<int64_t> VALUES;
 
-    vector<int> balTer(vals.size(), 0);
-    for (int i = 0; i < vals.size(); i++) 
-        for (int j = 0; j < 2; j++) 
-            if (inVal >= vals[i]) {
-                balTer[i]++;
-                inVal -= vals[i]; 
+        for (int i = 0; pow(3, i) <= ValueToBeConverted; i++) {
+            VALUES.push_back(int64_t(pow(3, i)));
+        }
+
+        sort(VALUES.begin(), VALUES.end(), std::greater<int64_t>());
+        
+        return VALUES;
+    }
+
+    std::vector<int> makeTernaryArray(const std::vector<int64_t> VALUES, int64_t valueToBeConverted) {
+        std::vector<int> ternaryArray(VALUES.size(), 0);
+
+        for (int i = 0; i < VALUES.size(); i++) { 
+            for (int j = 0; j < 2; j++) { 
+                if (abs(valueToBeConverted) >= VALUES[i]) {
+                    ternaryArray[i]++;
+                    valueToBeConverted = abs(valueToBeConverted) - VALUES[i]; 
+                }
             }
-    
-    for (int i = balTer.size() - 1; i >= 0; i--) {
-        if (balTer[i] == 3) {
-            balTer[i] = 0;
-            balTer[i-1]++;
-        } else {
-            if (balTer[i] == 2) {
-                balTer[i] = -1;
-                balTer[i-1]++;
-            }
+        }
+
+        return ternaryArray;
+    }
+
+    void convertTernaryArrayToBalancedTernary(std::vector<int> &ternaryArray) {
+        for (int i = ternaryArray.size() - 1; i > 0; i--) {
+            if (ternaryArray[i] == 3) {
+                ternaryArray[i] = 0;
+                ternaryArray[i-1]++;
+            } else if (ternaryArray[i] == 2) {
+                    ternaryArray[i-1]++; 
+                    ternaryArray[i] = -1;
+            } 
         }
     }
 
-    if (neg)
-        for (auto &i: balTer) 
-            if (i == 1) {
-                i = -1;
-            } else if (i == -1)
-                i = 1;
+    std::vector<char> convertToSymbols(const std::vector<int> &balancedTernaryArray, const bool invertSigns = false) {
+        std::vector<char> ans(balancedTernaryArray.size(), '0');
         
+        for (int i = 0; i < balancedTernaryArray.size(); i++) {
+            if (balancedTernaryArray[i] == 1) {
+                ans[i] = invertSigns ? '-' : '+';
+            } else if (balancedTernaryArray[i] == -1) 
+                ans[i] = invertSigns ? '+' : '-';
+        }
 
-    for (auto i: balTer)
-        cout << i << ' ';
-    return conVal;
-}
+        return ans;
+    }
+
+    void printAnswer(std::vector<char> ans) {
+        for (auto i : ans) {
+            std::cout << i;
+        }
+        std::cout << std::endl;
+    }
+
+public: 
+    template<typename T_Num>
+    void convert(T_Num valueToBeConverted) {
+        int64_t standardizedValueToBeConverted = int64_t(valueToBeConverted);
+        bool invertSigns = false;
+
+        if (standardizedValueToBeConverted < 0) {
+            invertSigns = true;
+        }
+
+        std::vector<int64_t> values = initalizeValues(standardizedValueToBeConverted);
+        
+        std::vector<int> ternaryArray = makeTernaryArray(values, standardizedValueToBeConverted);
+
+        convertTernaryArrayToBalancedTernary(ternaryArray);
+
+        std::vector<char> ans = convertToSymbols(ternaryArray, invertSigns);
+
+        printAnswer(ans);
+    }
+};
