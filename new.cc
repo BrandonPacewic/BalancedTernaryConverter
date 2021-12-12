3ded8fba7cb5c730198b9a94aad6558c6a0b08aa
Brandon <pacewicbrandon@gmail.com>
2021-12-11 21:51:11 -0800

    format changes

--- new.cc
+++ new.cc
@@ -1,6 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
 using namespace std;
 
+// fixed mod opperator
+// http://www.cplusplus.com/forum/general/19502/
 template<typename T, typename OP> struct modulo { T bace; }; enum { mod };
 template<typename T> modulo<T, decltype(mod)> operator<(const T& front, decltype(mod)) { return  { front }; }
 template<typename T> T operator>(modulo<T, decltype(mod)> tail, T exponent) { T& bace = tail.bace; return bace % exponent < 0 ? (bace % exponent) + exponent : bace % exponent; }
@@ -9,7 +12,6 @@ template<typename T> T abs(T num) { return num < 0 ? num * -1 : num; }
 
 class ternaryConverter {
 private:
-
     static string balancedTernary(int64_t n) {
         string output = "";
         bool invert = 0;
@@ -25,8 +27,10 @@ private:
                 rem = -1;
                 n++;
             }
+
             output = (invert ? rem == 1 ? '-' : rem == -1 ? '+' : '0' : rem == 1 ? '+' : rem == -1 ? '-' : '0') + output;
         }
+        
         return output;
     }
 
