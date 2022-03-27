c6717622ef8df51b7810d0ef9ec493dfc2d5f399
Brandon <pacewicbrandon@gmail.com>
2022-03-26 22:14:47 -0700

    added header and src for converter

---
+++ src/balanced_ternary_converter.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022 Brandon Pacewic
+ *
+ * Developed and tested by Brandon Pacewic
+ * 
+ * balanced_ternary_converter.h
+ */
+
+#pragma once
+#ifndef _BALANCED_TERNARY_CONVERTER_H
+#define _BALANCED_TERNARY_CONVERTER_H 1
+
+#include <string>
+
+namespace ternary {
+
+std::string balanced_convert(const int& value);
+
+} // namespace ternary   
+
+#endif // _BALANCED_TERNARY_CONVERTER_H
